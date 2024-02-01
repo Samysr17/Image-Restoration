95141612ca1e599af97df3151d11a86b447625c2
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2024-02-02 01:38:51 +0530

    image uploadation

--- client/img-restoration/src/Collage.jsx
+++ client/img-restoration/src/Collage.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useCallback } from 'react';
+import {useDropzone} from 'react-dropzone'
 import { FaPlusCircle } from "react-icons/fa";
 import Select from 'react-select';
+// import { ReactPhotoCollage } from "react-photo-collage";
 
 const Collage = () => {
     const options = [
@@ -9,6 +12,25 @@ const Collage = () => {
         { value: 'vanilla', label: 'Slider',color:'black'  },
         { value: 'Watermark Remover', label: 'Watermark Remover',color:'black'  }
       ]
+     
+      const [images,setimages]=useState([]);
+      const onDrop = useCallback(acceptedFiles => {
+        setimages(acceptedFiles.map(file=>
+          Object.assign(file,{
+            preview:URL.createObjectURL(file)
+          })
+       
+        ))
+        console.log(acceptedFiles);
+      }, [])
+      const {getRootProps, getInputProps} = useDropzone({onDrop});
+      const setting = {
+        // width: '600px',
+        // height: ['250px', '170px'],
+        // layout: [1, 4],
+        photos: images,
+        // showNumOfRemainingPhotos: false
+      };
   return (
     <div className="h-auto w-full bg-[#DFD5D5]">
     <div className="bg-[#1976D2] w-full h-auto">
@@ -25,13 +47,28 @@ const Collage = () => {
         </div>
         <div className="flex flex-col justify-center items-center">
            <div className="w-[40%] h-[500px] flex flex-col justify-center items-center mt-[5%] border-dashed border-2 border-black">
-             <FaPlusCircle size={20} className="mt-[5%]" />
-             <p>Drop your Images here</p>
+           <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          {images?.map(file=>{
+          <img className="w-50%" src={file.preview} alt="/"/>
+       })}
+       <div className="flex flex-col items-center ">
+        <FaPlusCircle size={20} className="mt-[5%]" /> 
+        <p>Drop</p>
+        </div>
+        <div className="grid grid-rows-2 gap-0 grid-flow-col">
+       {images?.map(file=>(
+          <img className="w-[50%]"  src={file.preview} alt="/"/>
+       ))}
+       </div>
+        </div>
+       
             
            </div>
            <div className="flex justify-center space-x-4 p-8">
              <button className="px-6 py-2 border-2 border-[#1976D2] bg-[#1976D2] text-white rounded-xl">Continue</button>
              <button className="px-6 py-2 border-2 border-[#1976D2] bg-[#1976D2] text-white rounded-xl">Discard</button>
+             {/* <ReactPhotoCollage {...setting} /> */}
            </div>
         </div>
         </div>
