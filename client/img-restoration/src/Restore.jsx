2ed875e1af0a343c19491d935b963570fa621198
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2024-03-25 20:26:39 +0530

    SUPER RESOLUTION DONE

--- client/img-restoration/src/Restore.jsx
+++ client/img-restoration/src/Restore.jsx
@@ -1,43 +1,29 @@
 import React from 'react'
 import * as tf from '@tensorflow/tfjs';
-import exampleImage from './assets/exampleImage.jpg';
-import Replicate from 'replicate';
+import exampleImage from './assets/esrgan3.png';
+import e2 from './assets/example_images_2.jpeg'
+import Upscaler from 'upscaler';
+
+// import Replicate from 'replicate';
 
 const Restore = () => {
-  //need to finalise numbers
-  //work on auth and stripe and backend
-  //watermark model as well
-        // const model = await tf.loadGraphModel('/model.json');
-    
-        // // Get content image
-        // let image = new Image(256,256);
-        // image.src = exampleImage;s
-    
-        // // Convert image to tensor and add batch dimension
-        // let tfTensor = tf.browser.fromPixels(image);    
-        // tfTensor = tfTensor.div(255.0);
-        // tfTensor = tfTensor.expandDims(0);
-        // tfTensor = tfTensor.cast("float32");
-        
-        // // Run image through model
-        // const pred = model.predict(tfTensor);
-        
-        // // Convert tensor to image
-        // let outputTensor = pred.squeeze();
-        
-        // // Scale to range [0,1] from [-1,1]
-        // outputTensor = outputTensor.mul(0.5);
-        // outputTensor = outputTensor.add(0.5);
-    
-        // // Prepare rendering of the result
-        // const canvas = document.getElementById('canvas'); 
-        // await tf.browser.toPixels(outputTensor, canvas);      
+  
+    const upscaler = new Upscaler();
+    const func=()=>{
+      upscaler.upscale(exampleImage).then(upscaledImage => {
+        const img = document.createElement("img")
+        img.src = upscaledImage
+        document.body.appendChild(img)
+        {    <div><img alt="" src={img}></img></div>}
+        console.log(upscaledImage);
+      });
     }
   return (
     <div>
-    <div>Restore</div>
+    <div onClick={func}>Restore</div>
     <div><img alt="" src={exampleImage}></img></div>
-    <button onClick={runModel()}>Continue</button>
+
+    {/* <button onClick={runModel()}>Continue</button> */}
     </div>
   )
 }
