e85e71f779cc9a7606f8077b9258ce92c2ee5efd
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2024-04-01 11:59:08 +0530

    user driven input for images done

--- client/img-restoration/src/Profile.jsx
+++ client/img-restoration/src/Profile.jsx
@@ -1,20 +1,66 @@
 import React from 'react'
-import { ReactPhotoCollage } from "react-photo-collage";
-import { useLocation } from "react-router-dom";
+import {useDropzone} from 'react-dropzone'
+import { useState } from 'react';
+import { useCallback } from 'react';
+import Upscaler from 'upscaler';
 
 const Profile = () => {
-  const location = useLocation();
-  const data = location.state;
-  const setting = {
-    width: '400px',
-    height: ['200px', '170px'],
-    layout: [1, 3],
-    photos: {data},
-  };
-  console.log({data});
+  const [images,setimages]=useState([]);
+  const[image,setimage]=useState("");
+  const [model,setmodel]=useState(false);
+  const [error,seterror]=useState("");
+  const upscaler=new Upscaler();
+  // upscaler.upscale(exampleImage).then(upscaledImage => {
+  //   const img = document.createElement("img")
+  //   img.src = upscaledImage
+  //   // document.body.appendChild(img)
+  //   setimage(img.src);
+  //   setmodel(!model);
+  //   console.log(upscaledImage);
+    
+  // });
+  const onDrop = useCallback(acceptedFiles => {
+    setimages(acceptedFiles.map(file=>
+      Object.assign(file,{
+        source:URL.createObjectURL(file)
+      })
+     
+    ));
+  }, [])
+  const {getRootProps, getInputProps,isDragActive} = useDropzone({onDrop});
+  const handleclick=()=>{
+      if(images.length>1){
+        seterror(error)
+        window.alert("Please Discard and select only 1 image");
+        return ;
+      }
+      images.map(file=>{
+        upscaler.upscale(file.source).then(upscaledImage => {
+          const img = document.createElement("img")
+          img.src = upscaledImage
+          // document.body.appendChild(img)
+          setimage(img.src);
+          setmodel(!model);
+          console.log(upscaledImage);
+        })
+      })
+  }
   return (
    <>
-  <ReactPhotoCollage  {...setting} />
+   <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+       {images?.map(file=>(
+          <img className="w-[20%]"  src={file.source} alt="/"/>
+          
+       ))}
+       <button onClick={handleclick} className="p-2 border-black border-solid">Restore</button>
+       {model?<img src={image} alt=""></img>:<div></div>}
+    </div>
    </>
   )
 }
